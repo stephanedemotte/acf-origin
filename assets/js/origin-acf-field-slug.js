f3a6ec4e8eb90c864ceb703e4cbf64efe60e31eb
Stephane Demotte <stephane@demotte.net>
2018-02-03 12:11:20 -0500

    fix js

--- assets/js/origin-acf-field-slug.js
+++ assets/js/origin-acf-field-slug.js
@@ -1,61 +1,65 @@
 (function($){
 
-  if(acf.field_group)
-    var acf_settings_slug = acf.field_group.field_object.extend({
+  acf.add_action('ready', function( $el ){
+    if(acf.field_group)
+      var acf_settings_slug = acf.field_group.field_object.extend({
+        type: 'slug',
+        actions: {
+          'render_settings': 'render',
+        },
+        render: function () {
+          var $choices = $('.acf-field-object-text, .acf-field-object-unique')
+          var $input = this.setting('origin_slug_select').find('select')
+
+          $input.off('select2:open')
+          $input.on('select2:open', this.render.bind(this))
+
+          $.each($choices, function(k, c) {
+            var data = acf.get_data($(c))
+            var name = $('#acf_fields-' + data.id +'-name').val()
+            var text = name + ' (' + data.type + ')'
+
+            if($input.find("option[value='" + data.key + "']").length)
+              return
+
+            var newOption = new Option(text, data.key, false, false)
+            $input.append(newOption).trigger('change')
+          })
+        }
+      })
+  })
+
+  acf.add_action('prepare', function( $el ){
+    acf.fields.slug = acf.field.extend({
       type: 'slug',
+      $input: null,
+      ref: null,
       actions: {
-        'render_settings': 'render',
+        'ready': 'render',
+        'append': 'render',
+      },
+      focus: function() {
+        this.$input = this.$field.find('input')
+        this.ref = this.$input.data('ref')
+        this.$input
+          .prop('readonly', true)
+          .css({ opacity: .7, backgroundColor: '#eee' })
+
+        this.$ref = $('input[name="acf[' + this.ref + ']"]')
       },
       render: function () {
-        var $choices = $('.acf-field-object-text, .acf-field-object-unique')
-        var $input = this.setting('origin_slug_select').find('select')
+        var $input = this.$input
 
-        $input.off('select2:open')
-        $input.on('select2:open', this.render.bind(this))
+        var onType = function() {
+          var value = acf.str_sanitize(this.value)
+          value = value.split('_').join('-')
+          $input.val(value)
+        }
 
-        $.each($choices, function(k, c) {
-          var data = acf.get_data($(c))
-          var name = $('#acf_fields-' + data.id +'-name').val()
-          var text = name + ' (' + data.type + ')'
-
-          if($input.find("option[value='" + data.key + "']").length)
-            return
-
-          var newOption = new Option(text, data.key, false, false)
-          $input.append(newOption).trigger('change')
-        })
+        this.$ref.off('input', onType)
+        this.$ref.on('input', onType)
       }
     })
-
-  acf.fields.slug = acf.field.extend({
-    type: 'slug',
-    $input: null,
-    ref: null,
-    actions: {
-      'ready': 'render',
-      'append': 'render',
-    },
-    focus: function() {
-      this.$input = this.$field.find('input')
-      this.ref = this.$input.data('ref')
-      this.$input
-        .prop('readonly', true)
-        .css({ opacity: .7, backgroundColor: '#eee' })
-
-      this.$ref = $('input[name="acf[' + this.ref + ']"]')
-    },
-    render: function () {
-      var $input = this.$input
-
-      var onType = function() {
-        var value = acf.str_sanitize(this.value)
-        value = value.split('_').join('-')
-        $input.val(value)
-      }
-
-      this.$ref.off('input', onType)
-      this.$ref.on('input', onType)
-    }
   })
 
 })(jQuery);
